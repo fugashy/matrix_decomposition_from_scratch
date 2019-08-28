be08ec2450daef3ab9007b8dccbfd2fd4299a7a9
fugashy <fugashy@icloud.com>
2019-08-28 22:02:56 +0900

    Improve member management

--- matrix_decomposition_from_scratch/matrices.py
+++ matrix_decomposition_from_scratch/matrices.py
@@ -2,46 +2,50 @@
 
 
 class Matrix():
-    def __init__(self, values):
-        if type(values) is not list:
-            raise TypeError('Matrix object requires list of values')
-        elif type(values[0]) is not list:
+    def __init__(self, data):
+        if type(data) is not list:
+            raise TypeError('Matrix object requires list of data')
+        elif type(data[0]) is not list:
             raise TypeError('Row of matrix should be list of value')
 
-        self.col = len(values[0])
-        for row_values in values:
-            if len(row_values) != self.col:
+        for row_data in data:
+            if len(row_data) != len(data[0]):
                 raise ValueError('All num of cols should be the same')
 
         # 全てfloat型にする
-        self._values = \
+        self.data = \
             [
                 [
-                    float(value) for value in row_values
+                    float(value) for value in row_data
                 ]
-                for row_values in values
+                for row_data in data
             ]
 
-        self.row = len(values)
+    @property
+    def row(self):
+        return len(self.data)
+
+    @property
+    def col(self):
+        return len(self.data[0])
 
     def __repr__(self):
         return 'Matrix()'
 
     def __str__(self):
         ret_str = '['
-        for ir, row_values in enumerate(self._values):
+        for ir, row_data in enumerate(self.data):
             if ir == 0:
                 ret_str += '[ '
             else:
                 ret_str += ' [ '
 
-            for cols_value in row_values:
+            for cols_value in row_data:
                 ret_str += str(cols_value) + ' '
 
-            if ir != len(self._values) - 1:
+            if ir != len(self.data) - 1:
                 ret_str += ']\n'
             else:
                 ret_str += ']'
         ret_str += ']'
         return ret_str
-
